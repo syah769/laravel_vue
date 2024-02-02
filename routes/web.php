06f44ca302b6120b7b281cdf9ca88f651a2a03e6
Syahril Ashraf <bajuhitam400@gmail.com>
2024-02-02 12:48:55 +0800

    Sec 4 - 2 SPA Authentication - Signing users in
    Add Sanctum middleware and login route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', \App\Http\Controllers\Auth\LoginController::class);
+});
